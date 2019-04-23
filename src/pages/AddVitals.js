3a57128ca4331dd6650b1012e6d4eec28eb4685b
atifrepo <syedatif32191@gmail.com>
2019-04-23 14:56:37 +0500

    search

--- src/pages/AddVitals.js
+++ src/pages/AddVitals.js
@@ -5,36 +5,38 @@ import { withStyles } from '@material-ui/core/styles';
 import NurseAppbar from '../NurseAppbar'
 import './addvitals.css'
 import Cookies from 'universal-cookie';
-const cookies=new Cookies();
+import toastr from 'toastr'
+import Button from '@material-ui/core/Button';
+const cookies = new Cookies();
 
 const styles = theme => ({
-  
+
   container: {
     display: 'flex',
     flexWrap: 'wrap',
   },
   textField: {
-    marginLeft: theme.spacing.unit  ,
+    marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
     // marginTop:theme.spacing.unit*4,
-    // width:'80%'
   },
   dense: {
+    // width:'80%'
     marginTop: 16,
   },
-  
+
   menu: {
     width: 200,
   },
-  textField1:{
-    marginLeft: theme.spacing.unit  ,
-    marginRight: theme.spacing.unit*9,
-  
+  textField1: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit * 9,
+
   }
-  
+
 });
 
- class AddVitals extends Component {
+class AddVitals extends Component {
 
   constructor() {
     super()
@@ -49,23 +51,71 @@ const styles = theme => ({
         Temprature: '',
         PO2: '',
         Allergies: '',
-      rolecookies:''
+        rolecookies: ''
       }
     }
 
   }
-componentDidMount(){
-  var rolecookis=cookies.get('roles')
-console.log('hi',rolecookis)
- this.setState({
-   rolecookies:rolecookis
- })
-}  
+  // componentDidMount() {
+  //   var rolecookis = cookies.get('roles')
+  //   console.log('hi', rolecookis)
+  //   this.setState({
+  //     rolecookies: rolecookis
+  //   })
+  // }
   handleClick() {
+
+    var Vitals = {
+      heights:this.state.Vitals.Height,
+      weight: this.state.Vitals.weight,
+      bloodpressure: this.state.Vitals.BP_Upper,
+      pulse: this.state.Vitals.Pulse,
+      tempreture: this.state.Vitals.Temprature,
+      po2: this.state.Vitals.PO2,
+      datetimes:'12-jan-2019',
+      allergiid:'1',
+      patientid:'5',
+    };
+    var formBody = [];
+    console.log("vitals values", Vitals);
+    for (var property in Vitals) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(Vitals[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    } 
+
+    formBody = formBody.join("&");
+
+    console.log("Form Body", formBody);
+    var Authtoken = cookies.get('token')
+    var finalAuthtoken = 'Bearer ' + Authtoken
+
+    fetch('http://ec2-54-198-188-131.compute-1.amazonaws.com:3000/addpatientvitals', {
+      method: 'POST',
+      withCredentials: true,
+
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'Authorization': finalAuthtoken
+
+      },
+      body: formBody
+
+    }).then(function (resp) {
+      if (resp.ok) {
+        toastr.options = {
+          positionClass: 'toast-top-right',
+          hideDuration: 3000,
+          timeOut: 100
+        }
+        toastr.clear()
+        setTimeout(() => toastr.success(`Patient registered`), 300)
+      }
+    })
 
     console.log('handle click login')
     console.log('user', this.state.Vitals)
-    this.props.history.push('/AddVitals')
+
   }
   handleChange(changeValue, event) {
     this.state.Vitals[changeValue] = event.target.value;
@@ -75,28 +125,35 @@ console.log('hi',rolecookis)
     })
     console.log('Register', this.state.Vitals)
   }
+  Search() {
+    console.log('searching')
+  }
 
   render() {
     const { classes } = this.props;
-  
-    return (
-      
-<div >
- 
- <div> 
 
-         
-        <h2 style={{color:'#2699FB'}}>Add Vitals of Patient</h2>
-          <NurseAppbar/>
+    return (
+
+      <div >
+
+        <div>
+
+
+          <h2 style={{ color: '#2699FB' }}>Add Vitals of Patient</h2>
+          <NurseAppbar />
           <form >
-            <TextField 
-              label="Enter MR_No"
-              value={this.state.MR_No}
-              onChange={this.handleChange.bind(this, 'MR_No')}
-              margin="normal"
-              variant="outlined"
-              className={classes.textField}
-            />
+            <div>
+              <TextField
+                label="Enter MR_No"
+                value={this.state.MR_No}
+                onChange={this.handleChange.bind(this, 'MR_No')}
+                margin="normal"
+                variant="outlined"
+                className={classes.textField}
+              />
+              <Button variant="outlined" style={{ backgroundColor: '#2699FB', marginTop: '2%' }} onClick={(event) => this.Search(event)}><b>Search</b></Button>
+            </div>
+            <br></br>
             <TextField
               label="Height"
               value={this.state.Height}
@@ -104,16 +161,17 @@ console.log('hi',rolecookis)
               variant="outlined"
               className={classes.textField}
               margin="normal"
-              />
-              <br></br>
-                     <TextField
+            />
+
+            <TextField
               label="Weight"
               value={this.state.Weight}
               onChange={this.handleChange.bind(this, 'Weight')}
               variant="outlined"
               className={classes.textField}
               margin="normal"
-         />
+            />
+            <br></br>
             <TextField
               label="BP(mmHg)"
               value={this.state.BP_Upper}
@@ -122,7 +180,6 @@ console.log('hi',rolecookis)
               className={classes.textField}
               margin="normal"
             />
-            <br></br>
             <TextField
               label="Pulse(bpm)"
               value={this.state.Pulse}
@@ -131,6 +188,8 @@ console.log('hi',rolecookis)
               className={classes.textField}
               margin="normal"
             />
+            <br></br>
+
             <TextField
               label="Tempreture"
               value={this.state.Temprature}
@@ -139,22 +198,24 @@ console.log('hi',rolecookis)
               className={classes.textField}
               margin="normal"
             />
-            <br></br>
-          <TextField
+
+            <TextField
               label="PO2"
               value={this.state.PO2}
               onChange={this.handleChange.bind(this, 'PO2')}
               variant="outlined"
               className={classes.textField}
               margin="normal"
-          />
-                </form>
-                  </div>
-                  
-                  </div>
-                
-   
-   )
+            />
+            <br></br>
+            <Button type="submit" variant="outlined" style={{ backgroundColor: '#2699FB', position: 'relative' }} onClick={(event) => this.handleClick(event)}><b>Add Vitals</b></Button>
+          </form>
+        </div>
+
+      </div>
+
+
+    )
   }
 }
 AddVitals.propTypes = {
